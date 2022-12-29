8e5c0c52c39d1d5fb443a9465f5d6c81198a73ff
Yang JianXin <995462226@qq.com>
2022-12-29 17:10:38 +0800

    add CaptionCollator

---
+++ component/datacollator.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, List
+import torch
+
+
+class CaptionCollator(object):
+    def __init__(self, tokenizer, max_seq_length):
+        self.tokenizer = tokenizer
+        self.max_seq_length = max_seq_length
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        captions, patch_images = [], []
+        for data in features:
+            captions.append(data['caption'])
+            patch_images.append(data['patch_image'])
+        # 获得encoder的输入
+        input_ids = self.tokenizer(
+            ['图片描述了什么?']*len(captions), return_tensors="pt", max_length=self.max_seq_length, truncation=True, padding=True
+        ).input_ids
+        patch_images = torch.concat(patch_images, dim=0)
+
+        # 获得decoder的输入
+        inputs = self.tokenizer(
+            captions, return_tensors="pt", max_length=self.max_seq_length, truncation=True, padding=True
+        )
+        decoder_input_ids = inputs.input_ids
+        attention_mask = inputs.attention_mask
+
+        inputs = {
+            'input_ids': input_ids,
+            'patch_images': patch_images,
+            'decoder_input_ids': decoder_input_ids,
+            'attention_mask': attention_mask,
+            'return_loss': True
+        }
+        return inputs
