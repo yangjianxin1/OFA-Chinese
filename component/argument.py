09413ff2abe6a8a3815249a3a0e723d0b011b350
Yang JianXin <995462226@qq.com>
2022-12-29 17:10:22 +0800

    add CaptionArguments

---
+++ component/argument.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class CaptionArguments:
+    """
+    自定义的一些参数
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+    max_seq_length: int = field(metadata={"help": "输入最大长度"})
+    train_caption_file: str = field(metadata={"help": "训练集"})
+    train_image_file: str = field(metadata={"help": "训练集"})
+    test_caption_file: str = field(metadata={"help": "测试集"})
+    test_image_file: str = field(metadata={"help": "测试集"})
+    model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
+
